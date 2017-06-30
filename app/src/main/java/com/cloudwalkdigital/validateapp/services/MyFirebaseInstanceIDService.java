667e8eeee8c2b6d000d6b47075f8b970c54cc3e1
Alleo Indong <freedom_war550@live.com.ph>
2017-06-30 12:45:06 +0800

    added fcm

---
+++ app/src/main/java/com/cloudwalkdigital/validateapp/services/MyFirebaseInstanceIDService.java
@@ -0,0 +1,12 @@
+package com.cloudwalkdigital.validateapp.services;
+
+import android.app.Service;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by alleoindong on 6/30/17.
+ */
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+}
---
+++ app/src/main/java/com/cloudwalkdigital/validateapp/services/MyFirebaseMessagingService.java
@@ -0,0 +1,12 @@
+package com.cloudwalkdigital.validateapp.services;
+
+import android.app.Service;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+/**
+ * Created by alleoindong on 6/30/17.
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+}
